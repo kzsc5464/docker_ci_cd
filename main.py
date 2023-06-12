7a0d20a73ac24b5f3142aee6bd54fdcebe696bb8
DESKTOP-L0CGF2I\kzsc5 <kzsc5464@naver.com>
2023-06-12 15:48:58 +0900

    test_githubAction

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello Bigger Applications!"}
 
-# @app.get("/testconflict")
-# async def root():
-#     return {"message": "이젠 넌 잘 수 있어!"}
+@app.get("/testconflict")
+async def root():
+    return {"message": "이젠 넌 잘 수 있어!"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
